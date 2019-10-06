d3f3c777f32a443f3b9b4399baa42f485719edcd
Joshua Martinez <joshkmartinez@gmail.com>
2019-10-05 22:05:37 -0700

    add special character and emoji support to trigger list

--- index.js
+++ index.js
@@ -3,39 +3,32 @@ const app = express();
 const { parse } = require("url");
 const axios = require("axios");
 require("dotenv").config();
-
 const port = 3000;
-
-// Body parser
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/triggers", (req, res) => {
   const { query } = parse(req.url, true);
-
   const { guild = "000000000000000000" } = query;
   axios
     .get(process.env.storage_service + guild)
     .then(async function(response) {
       if (JSON.stringify(response.data) == "{}") {
-        //no triggers set up
         res.end("This server does not has not created any triggers.");
       }
       let list=[]
       //compose array of all triggers
       for (i = 0; i < Object.keys(response.data).length; i++) {
         let j = Object.keys(response.data)[i];
-
-        //list.push("TRIGGER:  " + j + "   ~   RESPONSE:  " + response.data[j]);
-        list.push("TRIGGER:  " + j + "\nRESPONSE:  " + response.data[j]);
+        list.push("Trigger:  " + j + "<br/>Response:  " + response.data[j]);
       }
       let ans = "";
       for (i = 0; i < list.length; i++) {
-        ans += "Index: " + i + "\n" + list[i]+"\n\n";
+        ans += "Index: " + i + "<br/>" + list[i]+"<br/><br/>";
       }
-      res.end(ans);
+      res.send(ans);
     })
     .catch(async function(error) {
-      res.end("Error. This server does not exist.\n"+error);
+      res.end("Error. A server with that ID does not exist.\n"+error);
     });
 });
 
@@ -46,7 +39,7 @@ app.get("/invite", (req, res) => {
 });
 
 app.get("/", (req, res) => {
-  res.end("Hey check out this cool website for CuRe Bot!\nI know, this website is just so amazing you have an unfathomable urge to invite the bot to your discord server.\nWell here is how you can satisfy your urge. Go to https://cure.now.sh/invite")
+  res.end("Hey check out this cool website for CuRe Bot!\ntdlr: CuRe Bot is a custom response bot for discord.\nI know, this website is just so amazing you have an unfathomable urge to invite the bot to your discord server.\nWell here is how you can satisfy your urge. Go to https://cure.now.sh/invite")
 });
 
 app.listen(port, () => {
